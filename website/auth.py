6e916db76cece2c03dae06e736c888b4a257d333
dymasius <dymasius12@gmail.com>
2022-09-10 21:32:31 +0800

    created the login.html and sing_up.html linked to auth.py and views.py

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,12 @@
 # This is the root where user can have their view basically
 # it means thsi file has the blueprint of our app
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login<p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -14,4 +14,4 @@ def logout():
 
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign Up<p>"
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 # This is the root where user can have their view basically
 # it means thsi file has the blueprint of our app
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 # Basically this function will run when we go to the route '/'
 @views.route('/')
 def home():
-    return "<h1> Welcome to HOME!</h1>"
+    return render_template("home.html")
