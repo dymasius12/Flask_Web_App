601d7997c8136f3f355d0aefd61e3dc636ec5a27
dymasius <dymasius12@gmail.com>
2022-09-11 18:38:19 +0800

    finished the project for the note taking

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 # This is the root where user can have their view basically
 # it means thsi file has the blueprint of our app
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -25,3 +26,15 @@ def home():
             flash('Note is added', category='success')
     # we will be able to reference this current user and check if it is authenticated
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    
+    return jsonify({})
